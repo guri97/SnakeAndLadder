b54365f886e761cf56a7b846d2bc18981d4d55b0
guri97 <guri84096@gmail.com>
2022-06-10 10:37:21 +0530

    [gurpreet] UC3_PlayerPosition1

---
+++ SnakeAndLadder/SnakeAndLadder/PlayerPosition1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class PlayerPosition1
+    {
+        int[] arr = new int[100];
+        int[] snake = { 18, 54, 62, 64, 87, 95, 92, 98 };
+        int[] ladder = { 4, 9, 21, 28, 51, 71, 80 };//arr[18]=9
+        public void Positon2()
+        {
+            //for (int i = 0; i < arr.Length; i++)
+            //    if (i == 0)
+            //    {
+            //        Console.WriteLine("player is at the position Zero");
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("player not at position zero");
+            //    }
+            arr[0] = 0;
+            int Position = 0;
+            Console.WriteLine("player is at the position Zero");
+            for (int i = 0; i < 100; i++)
+            {
+                if (Position == 0)
+                {
+                    Dice roll = new Dice();
+                    roll.DiceRoll2();
+
+
+                }
+
+            }
+
+        }
+        public class Dice
+        {
+            public void DiceRoll2()
+
+            {
+
+                Console.WriteLine("enter 1 to roll the dice");
+                int Entered_val = Convert.ToInt32(Console.ReadLine());
+                if (Entered_val == 1)
+                {
+
+                    Random rand = new Random();
+                    int Dic = rand.Next(0, 7);
+                    Console.WriteLine(Dic);
+
+
+                }
+            }
+
+
+
+
+        }
+    }
+}
--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -4,14 +4,10 @@
     {
         public static void Main(string[] args)
         {
-            //uc2
-
-            //DiceRolls pos = new DiceRolls();
-            //pos.Positon1();
-            //pos.DiceRoll1();
-
-
-            //Console.WriteLine("Welcome to the Snake And Ladder");
+            //uc3
+            //PlayerPosition1 snakeAndLadder = new PlayerPosition1();
+            //snakeAndLadder.StartGame();
+            //Console.ReadLine();
 
         }
     }
