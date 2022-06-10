242dcc566fddb280fa440ab67d65dc903df20777
guri97 <guri84096@gmail.com>
2022-06-10 10:26:10 +0530

    [gurpreet] UC1_StartPositionZero

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SnakeAndLadder
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            //uc1
+            //StartPositionZero pos = new StartPositionZero();
+            //pos.Positon();
+
+        }
+    }
+}
---
+++ SnakeAndLadder/SnakeAndLadder/StartPositionZero.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class StartPositionZero
+    {
+        int[] arr = new int[100];
+        public void Positon()
+        {
+            //for (int i = 0; i < arr.Length; i++)
+            //if(i == 0)
+            //{
+            //    Console.WriteLine("player is at the position Zero");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("player not at position zero");
+            //}
+            arr[0] = 0;
+            Console.WriteLine("player is at the position Zero");
+
+        }
+
+    }
+}
